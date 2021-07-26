dfba6db672212e2634f2bba5327fb317d63dd467
meghraj1994 <meghrajprasadk@gmail.com>
2021-07-26 09:57:28 -0500

    typ

--- W1D6/ascii2.js
+++ W1D6/ascii2.js
@@ -1,5 +1,21 @@
 "use strict";
 var textArea, animDdl, sizeDdl, timer, startButton, stopButton, speed = 250, turboChk;
+window.onload = function () {
+    textArea = document.getElementById("text-area");
+    animDdl = document.getElementById("animation");
+    sizeDdl = document.getElementById("fontsize");
+    turboChk = document.getElementById("turbo");
+
+    startButton = document.getElementById("start");
+    stopButton = document.getElementById("stop");
+
+    startButton.onclick = start;
+    stopButton.onclick = stop;
+    animDdl.onchange = getFrames;
+    sizeDdl.onchange = setSize;
+    turboChk.onchange = turbo;
+
+}
 
 function start() {
     var frames = textArea.value.split("=====\n");
@@ -25,7 +41,7 @@ function stop(time) {
 
 
 function getFrames() {
-    var dropSel = animDdl.options[animDdl.selectedIndex].innerHTML;
+    var dropSel = animDdl.value;
     textArea.value = ANIMATIONS[dropSel];
 
 }
@@ -44,19 +60,4 @@ function turbo() {
     }
 }
 
-window.onload = function () {
-    textArea = document.getElementById("text-area");
-    animDdl = document.getElementById("animation");
-    sizeDdl = document.getElementById("fontsize");
-    turboChk = document.getElementById("turbo");
 
-    startButton = document.getElementById("start");
-    stopButton = document.getElementById("stop");
-
-    startButton.onclick = start;
-    stopButton.onclick = stop;
-    animDdl.onchange = getFrames;
-    sizeDdl.onchange = setSize;
-    turboChk.onchange = turbo;
-
-}
